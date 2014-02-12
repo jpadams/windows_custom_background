333793b6ac3d256bbc03dfa1caa903443d4fd139
jpadams <jeremy@puppetlabs.com>
2014-02-12 05:58:07 -0800

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,27 +1,24 @@
-class custom_script {
-  file {"make windows dir":
+class windows_custom_background {
+  
+  file { 'make images dir':
     ensure => directory,
-    path   => "C:\audit",
+    path   => 'C:/images',
   }
-  file {"make images dir":
-    ensure => directory,
-    path   => "C:\images",
-  }
-  file {"grab desktop background":
+  file { 'grab desktop background':
     ensure => present,
-    path   => "C:\images\maxresdefault.jpg",
-    source => "puppet:///modules/custom_script/maxresdefault.jpg",
-    require=> File["make images dir"],  
+    path   => 'C:/images/maxresdefault.jpg',
+    source => 'puppet:///modules/custom_script/maxresdefault.jpg',
+    require=> File['make images dir'],  
   }
-  file {"windows login script":
+  file { 'windows login script':
     ensure  => present,
-    path    => "C:\windows_script.bat",
-    source  => "puppet:///modules/custom_script/windows_script.bat",
-    require => File["make windows dir"],
+    path    => 'C:/windows_script.bat',
+    source  => 'puppet:///modules/custom_script/windows_script.bat',
+    require => File['make windows dir'],
   }
-  exec {"C:\windows_script.bat":
-    command  => "C:\windows_script.bat",
-    provider => "windows",
-    require  => File["windows login script","grab desktop background"],
+  exec { 'run the script':
+    command  => 'C:/windows_script.bat',
+    require  => File['windows login script','grab desktop background'],
   }
+  
 }
